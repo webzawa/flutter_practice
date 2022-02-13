cd17300eaa30c5f08b9309cbf1ddcdcaab254716
webzawa <35700309+webzawa@users.noreply.github.com>
2022-02-14 00:27:36 +0900

    Animation練習3

--- lib/main.dart
+++ lib/main.dart
@@ -24,13 +24,37 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  bool flag = false;
-
-  _click() async {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late AnimationController _animationControler;
+  _play() async {
     setState(() {
-      flag = !flag;
+      _animationControler.forward();
     });
+  }
+
+  _stop() async {
+    setState(() {
+      _animationControler.stop();
+    });
+  }
+
+  _reverse() async {
+    setState(() {
+      _animationControler.reverse();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _animationControler =
+        AnimationController(vsync: this, duration: Duration(seconds: 3));
+  }
+
+  @override
+  void dispose() {
+    _animationControler.dispose();
+    super.dispose();
   }
 
   @override
@@ -43,25 +67,24 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            AnimatedContainer(
-                duration: Duration(seconds: 3),
-                width: flag ? 100 : 50,
-                height: flag ? 50 : 100,
-                padding: flag ? EdgeInsets.all(0) : EdgeInsets.all(30),
-                margin: flag ? EdgeInsets.all(0) : EdgeInsets.all(30),
-                transform: flag ? Matrix4.skewX(0.0) : Matrix4.skewX(0.3),
-                color: flag ? Colors.blue : Colors.grey),
-            AnimatedSwitcher(
-                duration: Duration(seconds: 3),
-                child: flag
-                    ? Text("なにもない")
-                    : Icon(Icons.favorite, color: Colors.pink))
+            SizeTransition(
+              sizeFactor: _animationControler,
+              child: Center(
+                  child: SizedBox(
+                      width: 50,
+                      height: 50,
+                      child: Container(color: Colors.green))),
+            ),
           ],
         ),
       ),
       floatingActionButton:
-          Row(mainAxisAlignment: MainAxisAlignment.end, children: [
-        FloatingActionButton(onPressed: _click, child: Icon(Icons.add)),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        FloatingActionButton(
+            onPressed: _play, child: Icon(Icons.arrow_forward)),
+        FloatingActionButton(onPressed: _stop, child: Icon(Icons.pause)),
+        FloatingActionButton(
+            onPressed: _reverse, child: Icon(Icons.arrow_back)),
       ]),
     );
   }
